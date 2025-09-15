66a019d2878da15a8be2d1c079ae9ef5b8c92de6
Pedro Escapalete <pedroescapalete@gmail.com>
2025-09-15 08:01:20 -0300

    Finaliza a API Barbie

---
+++ models/dados.js
@@ -0,0 +1,34 @@
+const barbies = [
+  {
+    "id": 1,
+    nome: "Barbie Astronauta",
+    profissao: "Astronauta",
+    anoDeLancamento: 1965
+  },
+  {
+    "id": 2,
+    nome: "Barbie Médica",
+    profissao: "Médica",
+    anoDeLancamento: 1973
+  },
+  {
+    id: 3,
+    nome: "Barbie Presidente",
+    profissao: "Presidente",
+    anoDeLancamento: 2000
+  },
+  {
+    "id": 4,
+    nome: "Barbie Desenvolvedora de Jogos",
+    profissao: "Desenvolvedora de Jogos",
+    anoDeLancamento: 2016
+  },
+  {
+    "id": 5,
+    nome: "Barbie Cientista da Computação",
+    profissao: "Cientista da Computação",
+    anoDeLancamento: 2022
+  }
+]
+
+export default  { barbies };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import barbieRoutes from "./src/routes/barbieRoutes.js";
+import barbieRoutes from "./routes/barbieRoutes.js";
 
 const app = express();
 app.use(express.json());
